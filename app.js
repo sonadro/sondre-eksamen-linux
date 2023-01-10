1590f89ee89909d1b8dd434892620c7b29ec9d96
sonadro <sondre.rooth@gmail.com>
2023-01-10 10:36:02 +0100

    File upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+app.listen(3000);
+console.log('Connected!');
+
+// page routes
+app.get('/', (req, res) => res.redirect('/hjem'));
+app.get('/hjem', (req, res) => res.render('index'));
+app.get('/kart', (req, res) => res.render('kart'));
+app.get('/database', (req, res) => res.render('database'));
+
+// page not found
+app.use((req, res) => res.status(404).render('404'));
---
+++ public/database.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB3Wnnl-ENv0dGBLXTARFtpC7TJQk7403M",
+    authDomain: "eksamen-linux.firebaseapp.com",
+    databaseURL: "https://eksamen-linux.firebaseio.com",
+    projectId: "eksamen-linux",
+    storageBucket: "eksamen-linux.appspot.com",
+    messagingSenderId: "1051366519671"
+};
+firebase.initializeApp(config);
